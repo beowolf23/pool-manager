51c1114cceb7ae61134602b67dbaeaa071c74410
Mihai Ciocan <mihaiciocan44@gmail.com>
2024-11-08 14:54:44 +0200

    Refactored the handler logic to streamline the configuration and make the user not need to borrow any connection

--- src/main/java/org/beowolf23/App.java
+++ src/main/java/org/beowolf23/App.java
@@ -15,28 +15,19 @@ public class App
     {
         SSHJConfiguration sshConfiguration = new SSHJConfiguration("localhost", "22", "user", "bibi");
 
-        SSHJConnectionHandler handler = new SSHJConnectionHandler();
-
         ManagedConnectionPool<SSHJConfiguration, SSHJConnection> pool = new ManagedConnectionPoolBuilder<SSHJConfiguration, SSHJConnection>()
-                .withHandler(handler)
+                .withHandler(new SSHJConnectionHandler())
                 .maxActive(10)
-                .maxIdle(2)
+                .maxIdle(0)
                 .maxWaitTime(20)
-                .idleTime(20)
+                .idleTime(0)
                 .build();
 
-        // Borrow a connection from the pool
-        SSHJConnection sshConnection;
-        SSHJConnection sshConnection2;
-        SSHJConnection sshConnection3;
         GenericResponse<SSHJConnection> response;
         try {
-            sshConnection = pool.borrowObject(sshConfiguration);
-            handler.executeCommand(sshConnection, () -> "ls -l");
-            sshConnection2 = pool.borrowObject(sshConfiguration);
-            handler.executeCommand(sshConnection2, () -> "ls -l");
-            sshConnection3 = pool.borrowObject(sshConfiguration);
-            response = handler.executeCommand(sshConnection3, () -> "ls -l");
+            response = pool.executeCommand(sshConfiguration, () -> "ls -l");
+            Thread.sleep(1000);
+            System.out.println(pool.getNumIdle());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -55,23 +46,6 @@ public class App
                 response.getException().printStackTrace();
             }
         }
-
-        // Return the connection to the pool
-        System.out.println("Number of active connections: " + pool.getNumActive());
-        System.out.println("Number of idle connections: " + pool.getNumIdle());
-        pool.returnObject(sshConfiguration, sshConnection);
-        pool.returnObject(sshConfiguration, sshConnection2);
-        pool.returnObject(sshConfiguration, sshConnection3);
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        System.out.println("=================================");
-        System.out.println("Number of active connections: " + pool.getNumActive());
-        System.out.println("Number of idle connections: " + pool.getNumIdle());
 
         // Shutdown the pool
         pool.close();
